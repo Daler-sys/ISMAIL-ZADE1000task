37311abf68a3b671b708e6c646e84254540070f0
Daler Ismail-zade <ismailzade.d.n@gmail.com>
2026-09-14 12:46:50 +0300

    Create HW.py

---
+++ HW.py
@@ -0,0 +1,33 @@
+#a = int(input())
+#b = int(input())
+#i = a
+#s = 0
+#for i in range(a, b):
+#        s += i
+#print(s)
+
+#a = int(input())
+#b = int(input())
+#i = a
+#s = 1
+#for i in range(a, b):
+#        s *= i
+#print(s)
+
+#a = int(input())
+#b = int(input())
+#i = a
+#s = 1
+#for i in range(a, b):
+#        s += i**2
+#print(s)
+
+#n = int(input())
+#s = 0
+#for i in range(1, n):
+#        s += 1/n
+#print(s)
+
+#n = int(input())
+#s = 0
+#for i in range(1, n):
